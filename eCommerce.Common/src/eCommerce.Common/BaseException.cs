3a6f2fa174f59fe74b45fb1316f5ef061a4d0a09
ilya02il <ilya02il@yandex.ru>
2024-02-04 19:16:25 +0700

    Добавил базовые классы библиотки Common и тесты к ним.

---
+++ eCommerce.Common/src/eCommerce.Common/BaseException.cs
@@ -0,0 +1,95 @@
+﻿using System.Text.RegularExpressions;
+
+namespace eCommerce.Common;
+
+/// <summary>
+/// Базовый класс для всех исключений в проектах
+/// (использовать как базовый класс вместо <see cref="Exception"/>).
+/// </summary>
+public abstract partial class BaseException : Exception
+{
+    [GeneratedRegex(@"^[A-ZА-Я]{1,}-\d{1,}$", RegexOptions.Compiled)]
+    private static partial Regex GetDefaultErrorCodeRegex();
+
+    private static string GetFormattedErrorMessage(string errorCode, string errorMessage)
+    {
+        return $"{errorCode}: {errorMessage}";
+    }
+
+    /// <summary>
+    /// Регулярное выражения для строки кода ошибки.
+    /// </summary>
+    protected virtual Regex ErrorCodeRegex => GetDefaultErrorCodeRegex();
+
+    /// <summary>
+    /// Код ошибки.
+    /// </summary>
+    public ErrorCode ErrorCode { get; }
+
+    /// <summary>
+    /// Сообщение об ошибке.
+    /// </summary>
+    public string ErrorMessage { get; }
+
+    /// <summary>
+    /// Создать новый экземпляр исключения,
+    /// используя код ошибки и другое исключение.
+    /// </summary>
+    /// <param name="errorCode">
+    /// Код ошибки.
+    /// </param>
+    /// <param name="referencedException">
+    /// Другое исключение, сообщение которого будет
+    /// взято за основу для создаваемого исключения.
+    /// </param>
+    public BaseException(string errorCode, Exception referencedException)
+        : base(GetFormattedErrorMessage(errorCode, referencedException.Message))
+    {
+        ErrorCode = new ErrorCode(errorCode, ErrorCodeRegex);
+        ErrorMessage = referencedException.Message;
+    }
+
+    /// <summary>
+    /// Создать новый экземпляр исключения,
+    /// используя код ошибки и сообщение об ошибке.
+    /// </summary>
+    /// <param name="errorCode">
+    /// Код ошибки.
+    /// </param>
+    /// <param name="errorMessage">
+    /// Сообщение об ошибке.
+    /// </param>
+    public BaseException(string errorCode, string errorMessage)
+        : base(GetFormattedErrorMessage(errorCode, errorMessage))
+    {
+        ErrorCode = new ErrorCode(errorCode, ErrorCodeRegex);
+        ErrorMessage = errorMessage;
+    }
+
+    /// <summary>
+    /// Создать новый экземпляр исключения,
+    /// используя код ошибки, сообщение об ошибке и
+    /// исключение, которое станет вложенным для создаваемого.
+    /// </summary>
+    /// <param name="errorCode">
+    /// Код ошибки.
+    /// </param>
+    /// <param name="errorMessage">
+    /// Сообщение об ошибке.
+    /// </param>
+    /// <param name="innerException">
+    /// Иключение, которое станет вложенным для создаваемого.
+    /// </param>
+    public BaseException(
+        string errorCode,
+        string errorMessage,
+        Exception innerException
+    ) : base(
+        GetFormattedErrorMessage(errorCode, errorMessage),
+        innerException
+    )
+    {
+        ErrorCode = new ErrorCode(errorCode, ErrorCodeRegex);
+        ErrorMessage = errorMessage;
+    }
+}
---
+++ eCommerce.Common/tests/eCommerce.Common.Tests/ErrorCodeTests.cs
@@ -0,0 +1,139 @@
+﻿using System.Text.RegularExpressions;
+
+namespace eCommerce.Common.Tests;
+
+public class ErrorCodeTests
+{
+    private static readonly Regex CommonErrorCodeRegex = new(
+        pattern: @"^COMMON-[A-ZА-Я-]{1,}[-]?(?>\d{1,})?$",
+        options: RegexOptions.Compiled
+    );
+
+    [Fact]
+    public void Create_An_Error_Code_With_Invalid_Format_Should_Throw_COMMON_ERR_001()
+    {
+        // Arrange
+        var errorCodeString = "ddd-83";
+
+        // Act
+        var errorCodeCreation = () => new ErrorCode(errorCodeString, CommonErrorCodeRegex);
+
+        // Assert
+        errorCodeCreation
+            .Should()
+            .Throw<CommonException>()
+            .Which
+            .ErrorCode
+            .Should()
+            .Be("COMMON-ERRCODE-001");
+    }
+
+    [Fact]
+    public void ToString_Should_Returns_An_Error_Code_String()
+    {
+        // Arrange
+        var errorCodeString = "COMMON-ERR-001";
+        var errorCode = new ErrorCode(errorCodeString, CommonErrorCodeRegex);
+
+        // Act
+        var toStringResult = errorCode.ToString();
+
+        // Assert
+        toStringResult
+            .Should()
+            .Be(errorCodeString);
+    }
+
+    [Fact]
+    public void Implicit_To_String_Conversion_Result_Should_Be_Equals_To_Error_Code_String()
+    {
+        // Arrange
+        var errorCodeString = "COMMON-ERR-001";
+        var errorCode = new ErrorCode(errorCodeString, CommonErrorCodeRegex);
+
+        // Act
+        string implicitToStringResult = errorCode;
+
+        // Assert
+        implicitToStringResult
+            .Should()
+            .Be(errorCodeString);
+    }
+
+    [Fact]
+    public void Equals_And_Equal_Operator_Results_Should_Be_True()
+    {
+        // Arrange
+        var errorCodeString = "COMMON-ERR-001";
+        var firstErrorCode = new ErrorCode(errorCodeString, CommonErrorCodeRegex);
+        var secondErrorCode = new ErrorCode(errorCodeString, CommonErrorCodeRegex);
+
+        // Act
+        var errorCodesEqualsResult = firstErrorCode.Equals(secondErrorCode);
+        var errorStringEqualsResule = firstErrorCode.Equals(errorCodeString);
+        var equalOperatorResult = firstErrorCode == secondErrorCode;
+
+        // Arrange
+        errorCodesEqualsResult
+            .Should()
+            .BeTrue();
+
+        errorStringEqualsResule
+            .Should()
+            .BeTrue();
+
+        equalOperatorResult
+            .Should()
+            .BeTrue();
+    }
+
+    [Fact]
+    public void Equals_Result_Should_Be_False_And_Not_Equal_Operator_Result_Should_Be_True()
+    {
+        // Arrange
+        var firstErrorCodeString = "COMMON-ERR-001";
+        var secondErrorCodeString = "COMMON-ERR-002";
+        var firstErrorCode = new ErrorCode(firstErrorCodeString, CommonErrorCodeRegex);
+        var secondErrorCode = new ErrorCode(secondErrorCodeString, CommonErrorCodeRegex);
+
+        // Act
+        var errorCodesEqualsResult = firstErrorCode.Equals(secondErrorCode);
+        var errorStringEqualsResult = firstErrorCode.Equals(secondErrorCodeString);
+        var differentTypesEqualsResult = firstErrorCode.Equals(default(int));
+        var notEqualOperatorResult = firstErrorCode != secondErrorCode;
+
+        // Assert
+        errorCodesEqualsResult
+            .Should()
+            .BeFalse();
+
+        errorStringEqualsResult
+            .Should()
+            .BeFalse();
+
+        differentTypesEqualsResult
+            .Should()
+            .BeFalse();
+
+        notEqualOperatorResult
+            .Should()
+            .BeTrue();
+    }
+
+    [Fact]
+    public void GetHashCode_Result_Should_Be_Equals_To_Error_String_GetHashCode_Result()
+    {
+        // Arrange
+        var errorCodeString = "COMMON-ERR-001";
+        var errorCode = new ErrorCode(errorCodeString, CommonErrorCodeRegex);
+
+        // Act
+        var errorCodeStringHashCode = errorCodeString.GetHashCode();
+        var errorCodeHashCode = errorCode.GetHashCode();
+
+        // Assert
+        errorCodeStringHashCode
+            .Should()
+            .Be(errorCodeHashCode);
+    }
+}
