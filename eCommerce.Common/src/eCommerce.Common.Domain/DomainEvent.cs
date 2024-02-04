1711c2ccd6051236bcde82b71bf904bdabce85b1
ilya02il <ilya02il@yandex.ru>
2024-02-04 19:28:26 +0700

    Добавил базовые классы библиотки Common.Domain и тесты к ним.

---
+++ eCommerce.Common/src/eCommerce.Common.Domain/DomainEvent.cs
@@ -0,0 +1,24 @@
+﻿namespace eCommerce.Common.Domain;
+
+/// <summary>
+/// Базовый класс для событий предметой области.
+/// </summary>
+/// <typeparam name="TAggregateId">
+/// Тип идентификатора агрегата.
+/// </typeparam>
+public abstract record DomainEvent<TAggregateId> : Event
+{
+    /// <summary>
+    /// Идентификатор агрегата,
+    /// который опубликовал событие предметной области.
+    /// </summary>
+    public required TAggregateId AggregateRootId { get; init; }
+
+
+    /// <inheritdoc/>
+    public DomainEvent() : base() { }
+
+    /// <inheritdoc/>
+    public DomainEvent(TimeProvider timeProvider)
+        : base(timeProvider) { }
+}
