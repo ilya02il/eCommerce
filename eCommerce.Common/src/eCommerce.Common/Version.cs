75a9288ac331fd605519be98e55b211f1372d179
ilya02il <ilya02il@yandex.ru>
2024-03-30 20:33:52 +0700

    Добавил класс семантической версии.

---
+++ eCommerce.Common/src/eCommerce.Common/Version.cs
@@ -0,0 +1,114 @@
+﻿using System.Text.RegularExpressions;
+
+namespace eCommerce.Common;
+
+/// <summary>
+/// Семантическая версия чего-либо.
+/// </summary>
+public sealed partial record Version
+{
+    [GeneratedRegex(@"^[\w\d\-.]*$", RegexOptions.Compiled)]
+    private static partial Regex GetPostfixRegex();
+
+    private readonly string? _postfix;
+
+    /// <summary>
+    /// Мажорная версия. Поднимается если были сделаны
+    /// глобальные обратно-несовместимые изменения.
+    /// </summary>
+    public uint Major { get; private init; }
+
+    /// <summary>
+    /// Минорная версия. Поднимается если были сделаны
+    /// небольшие обратно-совместимые изменения.
+    /// </summary>
+    public uint Minor { get; private init; }
+
+    /// <summary>
+    /// Патч-версия. Поднимается если были
+    /// исправлены какие-либо баги.
+    /// </summary>
+    public uint Patch { get; private init; }
+
+    /// <summary>
+    /// Билд-версия. Поднимается после каждого билда
+    /// новой версии при разработке.
+    /// </summary>
+    public uint Build { get; private init; }
+
+    /// <summary>
+    /// Постфикс версии. Добавляет описание к версии в конце,
+    /// например о том, что данная версия является предрелизной.
+    /// </summary>
+    public string? Postfix
+    {
+        get => _postfix;
+        private init
+        {
+            if (value is not null && GetPostfixRegex().IsMatch(value) is false)
+            {
+                throw new CommonException(
+                    errorCode: "COMMON-VERSION-001",
+                    message:
+                        "Некорректный формат постфикса версии. " +
+                        "Постфикс может содержать только латинские буквы, " +
+                        "цифры и следующие знаки: '-', '.'."
+                );
+            }
+
+            _postfix = value;
+        }
+    }
+
+    /// <summary>
+    /// Создать новый экземпляр семантической версии.
+    /// </summary>
+    /// <param name="major">
+    /// Мажорная версия. Поднимается если были сделаны
+    /// глобальные обратно-несовместимые изменения.
+    /// </param>
+    /// <param name="minor">
+    /// Минорная версия. Поднимается если были сделаны
+    /// небольшие обратно-совместимые изменения.
+    /// </param>
+    /// <param name="patch">
+    /// Патч-версия. Поднимается если были
+    /// исправлены какие-либо баги.
+    /// </param>
+    /// <param name="build">
+    /// Билд-версия. Поднимается после каждого билда
+    /// новой версии при разработке.
+    /// </param>
+    /// <param name="postfix">
+    /// Постфикс версии. Добавляет описание к версии в конце,
+    /// например о том, что данная версия является предрелизной.
+    /// </param>
+    public Version(
+        uint major,
+        uint minor = 0,
+        uint patch = 0,
+        uint build = 0,
+        string? postfix = null
+    )
+    {
+        Major = major;
+        Minor = minor;
+        Patch = patch;
+        Build = build;
+        Postfix = postfix;
+    }
+
+    /// <inheritdoc />
+    public override string ToString()
+    {
+        var stringifiedVersion =
+            string.Join(".", Major, Minor, Patch, Build);
+
+        if (string.IsNullOrEmpty(Postfix))
+        {
+            return stringifiedVersion;
+        }
+
+        return $"{stringifiedVersion}-{Postfix}";
+    }
+}
---
+++ eCommerce.Common/tests/eCommerce.Common.Tests/VersionTests.cs
@@ -0,0 +1,174 @@
+﻿namespace eCommerce.Common.Tests;
+
+public class VersionTests
+{
+    [Fact]
+    public void Versions_Should_Create_Correctly()
+    {
+        // Arrange
+        var fullVersion = new Version(
+            major: 1, 
+            minor: 2,
+            patch: 3,
+            build: 4,
+            postfix: "five"
+        );
+
+        var versionWithoutPostfix = new Version(
+            major: 1,
+            minor: 2,
+            patch: 3,
+            build: 4
+        );
+
+        var versionWithoutPostfixAndBuild = new Version(
+            major: 1,
+            minor: 2,
+            patch: 3
+        );
+
+        var versionWithOnlyMajorAndMinor = new Version(
+            major: 1,
+            minor: 2
+        );
+
+        var versionWithOnlyMajor = new Version(major: 1);
+
+        // Assert
+        new[]
+        {
+            fullVersion,
+            versionWithoutPostfix,
+            versionWithoutPostfixAndBuild,
+            versionWithOnlyMajorAndMinor,
+            versionWithOnlyMajor
+        }
+        .Should()
+        .AllSatisfy((version) => version.Major.Should().Be(1));
+        
+        new[]
+        {
+            fullVersion,
+            versionWithoutPostfix,
+            versionWithoutPostfixAndBuild,
+            versionWithOnlyMajorAndMinor
+        }
+        .Should()
+        .AllSatisfy((version) => version.Minor.Should().Be(2));
+        
+        new[]
+        {
+            fullVersion,
+            versionWithoutPostfix,
+            versionWithoutPostfixAndBuild
+        }
+        .Should()
+        .AllSatisfy((version) => version.Patch.Should().Be(3));
+        
+        new[]
+        {
+            fullVersion,
+            versionWithoutPostfix
+        }
+        .Should()
+        .AllSatisfy((version) => version.Build.Should().Be(4));
+
+        fullVersion
+            .Postfix
+            .Should()
+            .Be("five");
+    }
+
+    [Fact]
+    public void Versions_Should_Be_Equal()
+    {
+        // Arrange
+        var firstVersion = new Version(
+            major: 1,
+            minor: 2,
+            patch: 3,
+            build: 4,
+            postfix: "five"
+        );
+
+        var secondVersion = new Version(
+            major: 1,
+            minor: 2,
+            patch: 3,
+            build: 4,
+            postfix: "five"
+        );
+
+        // Act
+        var equalsResult = firstVersion.Equals(secondVersion);
+        var equalityOperatorResult = firstVersion == secondVersion;
+
+        // Assert
+        equalsResult.Should().BeTrue();
+        equalityOperatorResult.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Versions_Should_Not_Be_Equal()
+    {
+        // Arrange
+        var firstVersion = new Version(
+            major: 1,
+            minor: 2,
+            patch: 3,
+            build: 4,
+            postfix: "five"
+        );
+
+        var secondVersion = new Version(
+            major: 1,
+            minor: 2,
+            patch: 3,
+            build: 4
+        );
+
+        // Act
+        var equalsResult = firstVersion.Equals(secondVersion);
+        var equalityOperatorResult = firstVersion == secondVersion;
+
+        // Assert
+        equalsResult.Should().BeFalse();
+        equalityOperatorResult.Should().BeFalse();
+    }
+
+    [Fact]
+    public void Version_ToString_Should_Return_Correct_String()
+    {
+        // Arrange
+        var versionWithoutPostfix = new Version(
+            major: 1,
+            minor: 2,
+            patch: 3,
+            build: 4
+        );
+        
+        var versionWithPostfix = new Version(
+            major: 1,
+            minor: 2,
+            patch: 3,
+            build: 4,
+            postfix: "five"
+        );
+
+        // Act
+        var stringifiedVersionWithoutPostfix =
+            versionWithoutPostfix.ToString();
+
+        var stringifiedVersionWithPostfix =
+            versionWithPostfix.ToString();
+
+        // Assert
+        stringifiedVersionWithoutPostfix
+            .Should()
+            .Be("1.2.3.4");
+
+        stringifiedVersionWithPostfix
+            .Should()
+            .Be("1.2.3.4-five");
+    }
+}
