7b71704bc0f2196be9e789053180d5dba7f7bd30
Ilya Ilinykh <ilya02il@yandex.ru>
2024-08-02 11:10:34 +0700

    Убрал логику по созданию события из Event, сделал свойства Id и DateStamp required

--- eCommerce.Common/src/eCommerce.Common/Event.cs
+++ eCommerce.Common/src/eCommerce.Common/Event.cs
@@ -6,25 +6,12 @@
 public abstract record Event
 {
     /// <summary>
-    /// Создать событие,
-    /// в качестве провайдера для инциализации <see cref="DateStamp"/>
-    /// которого будет использоваться <paramref name="timeProvider"/>.
-    /// </summary>
-    /// <param name="timeProvider">
-    /// Провайдер для инициализации <see cref="DateStamp"/>.
-    /// </param>
-    protected Event(TimeProvider? timeProvider = null)
-    {
-        DateStamp = (timeProvider ?? TimeProvider.System).GetUtcNow();
-    }
-
-    /// <summary>
     /// Идентификатор события.
     /// </summary>
-    public Guid Id { get; } = Guid.NewGuid();
+    public required Guid Id { get; init; }
 
     /// <summary>
     /// Дата создания события.
     /// </summary>
-    public DateTimeOffset DateStamp { get; }
+    public required DateTimeOffset DateStamp { get; init; }
 }
